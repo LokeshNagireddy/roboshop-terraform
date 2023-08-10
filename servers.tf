c62389b462db4df64eae4bc46b94528362fde2b0
lokesh.nagireddy <lokesh.nagireddy@accenture.com>
2023-08-10 16:51:07 +0530

    terraform roboshop

--- servers.tf
+++ servers.tf
@@ -1,21 +1,19 @@
-module "database_servers" {
+module "database-servers" {
   for_each = var.database_servers
 
   source = "./module"
-
-  server_name=each.value["name"]
-  env = var.env
+  server_name = each.value["name"]
+  env         = var.env
   instance_type = each.value["instance_type"]
-  password = lookup(each.value, "password", "null")
-  provisioner = true
+  password      = lookup(each.value, "password", "null")
+  provisioner   = true
 }
 
 module "app_servers" {
-  depends_on = [module.database_servers]
-  for_each = var.app_servers
+  depends_on = [module.database-servers]
+  for_each   = var.app_servers
 
   source = "./module"
-
   server_name=each.value["name"]
   env = var.env
   instance_type = each.value["instance_type"]
