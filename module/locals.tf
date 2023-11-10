bab8000f56bbd6fdbf0c6e1f8d406b1c2150081c
lokesh.nagireddy <lokesh.nagireddy@accenture.com>
2023-11-10 10:59:30 +0530

    Terraform locals - practice

--- module/locals.tf
+++ module/locals.tf
@@ -1,3 +1,14 @@
 locals {
   name = var.env != "" ? "${var.server_name}-${var.env}" : var.server_name
+  db_commands = [
+    "rm -rf roboshop-shell",
+    "git clone https://github.com/LokeshNagireddy/roboshop-shell.git",
+    "cd roboshop-shell",
+    "sudo bash ${var.server_name}.sh ${var.password}"
+  ]
+  app_commands = [
+    "sudo labauto ansible",
+    "ansible pull -i localhost, -U https://github.com/LokeshNagireddy/roboshop-ansible.git roboshop.yml -e env=${var.env} -e role_name=${var.server_name}"
+
+  ]
 }
--- vars.tf
+++ vars.tf
@@ -1,3 +1,4 @@
 variable "database_servers" {}
 variable "app_servers" {}
 variable "env" {}
+variable "app_type" {}
